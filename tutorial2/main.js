e0da1cca9f992af24219fa2d520ae27f778577e0
Jorge Plasencia <66725751+WaberHoruhe@users.noreply.github.com>
2022-01-29 14:43:42 -0700

    test

--- tutorial2/main.js
+++ tutorial2/main.js
@@ -27,7 +27,7 @@ const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
 const material = new THREE.MeshStandardMaterial({
   color: 0xff6347,
 });
-const   torus = new THREE.Mesh(geometry, material);
+const torus = new THREE.Mesh(geometry, material);
 
 //scene.add(torus);
 
@@ -74,17 +74,16 @@ function animate() {
   renderer.render(scene, camera);
 }
 
-const moonTexture = new THREE.TextureLoader().load("moon.jpeg")
-const normalTexture = new THREE.TextureLoader().load("normal.jpeg")
+const moonTexture = new THREE.TextureLoader().load("moon.jpeg");
+const normalTexture = new THREE.TextureLoader().load("normal.jpeg");
 
 const moon = new THREE.Mesh(
-  new THREE.SphereGeometry(3,32,32),
+  new THREE.SphereGeometry(3, 32, 32),
   new THREE.MeshStandardMaterial({
     map: moonTexture,
     normalMap: normalTexture,
   })
-)
-scene.add(moon)
-
+);
+scene.add(moon);
 
 animate();
